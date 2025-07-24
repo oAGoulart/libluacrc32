bc3e3307af0bc37c531fed70e44aeb7da06897bd
Augusto Goulart <26435937+oAGoulart@users.noreply.github.com>
2025-07-24 08:55:25 -0300

    Move lookup tables to lookup.c

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 targ='libluacrc32'
 
 gcc -Wall -Wextra -Wpedantic -Wshadow -Wformat=2 \
-    -fPIC $CFLAGS --std=c11 -O2 -c $targ.c
+    -fPIC $CFLAGS --std=c11 -O2 -c lookup.c $targ.c
 
 if [ "$(printenv 'OS')" = 'Windows_NT' ]; then
-  gcc -shared -lm -o $targ.dll $targ.o $LFLAGS -llua54
+  gcc -shared -lm -o $targ.dll $targ.o lookup.o $LFLAGS -llua54
 else
-  gcc -shared -lm -o $targ.so $targ.o $LFLAGS
+  gcc -shared -lm -o $targ.so $targ.o lookup.o $LFLAGS
 fi
